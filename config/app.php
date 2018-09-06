ad9d352017da13467140e278c6acf61cc33a2643
Gaurang Kumar <gaurangkumarp@gmail.com>
2018-09-06 21:27:04 +0530

    Create app.php

---
+++ config/app.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * IronPHP : PHP Development Framework
+ * Copyright (c) IronPHP (https://github.com/IronPHP/IronPHP)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @package       IronPHP
+ * @copyright     Copyright (c) IronPHP (https://github.com/IronPHP/IronPHP)
+ * @link          
+ * @since         0.0.1
+ * @license       MIT License (https://opensource.org/licenses/mit-license.php)
+ * @auther        Gaurang Parmar <gaurangkumarp@gmail.com>
+ */
+
+return [
+
+    /**
+     *--------------------------------------------------------------------------
+     * Application Name
+     *--------------------------------------------------------------------------
+     *
+     * This value is the name of your application. This value is used when the
+     * framework needs to place the application's name in a notification or
+     * any other location as required by the application or its packages.
+     *
+     */
+
+    'name' => 'IronPHP',
+
+    /**
+     *--------------------------------------------------------------------------
+     * Application Environment
+     *--------------------------------------------------------------------------
+     *
+     * This value determines the "environment" your application is currently
+     * running in.
+     *
+     */
+
+    'env' => 'production',
+
+    /**
+     *--------------------------------------------------------------------------
+     * Application Debug Mode
+     *--------------------------------------------------------------------------
+     *
+     * When your application is in debug mode, detailed error messages with
+     * stack traces will be shown on every error that occurs within your
+     * application. If disabled, a simple generic error page is shown.
+     *
+     */
+
+    'debug' => false,
+
+    /**
+     *--------------------------------------------------------------------------
+     * Application URL
+     *--------------------------------------------------------------------------
+     *
+     * This URL is used by the console to properly generate URLs when using
+     * the Jarvis command line tool.
+     *
+     */
+
+    'url' => 'http://localhost',
+
+    /**
+     *--------------------------------------------------------------------------
+     * Application Timezone
+     *--------------------------------------------------------------------------
+     *
+     * Here you may specify the default timezone for your application, which
+     * will be used by the PHP date and date-time functions.
+     *
+     */
+
+    'timezone' => 'UTC',
+
+    /**
+     *--------------------------------------------------------------------------
+     * Application Locale Configuration
+     *--------------------------------------------------------------------------
+     *
+     * The application locale determines the default locale.
+     *
+     */
+
+    'locale' => 'en',
+
+];
