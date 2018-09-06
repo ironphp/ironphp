cd8bbef8073ed76b1523259901ce2bb3e8a069ad
Gaurang Kumar <gaurangkumarp@gmail.com>
2018-09-06 21:44:07 +0530

    Create Command.php

---
+++ src/Friday/Console/Command.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * IronPHP : PHP Development Framework
+ * Copyright (c) IronPHP (https://github.com/IronPHP/IronPHP)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @package       IronPHP
+ * @copyright     Copyright (c) IronPHP (https://github.com/IronPHP/IronPHP)
+ * @link          
+ * @since         0.0.1
+ * @license       MIT License (https://opensource.org/licenses/mit-license.php)
+ * @auther        Gaurang Parmar <gaurangkumarp@gmail.com>
+ */
+
+namespace Friday\Console;
+
+class Command
+{
+
+    /**
+     * Argv Inputs from console parameters.
+     *
+     * @var
+     */
+    protected $argvInput;
+
+    /**
+     * Commands instance.
+     *
+     * @var
+     */
+    protected $commands;
+
+    /**
+     * Create a new console command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+        $app = new \Friday\Foundation\Application();
+        
+        $this->argvInput = ($_SERVER['argv'][0] === "jarvis") ? array_slice($_SERVER['argv'], 1) : [] ;
+        
+        // get all commands
+        $this->commands = new \Friday\Console\Commands($app->version());
+        
+        // run commands
+        if(count($this->argvInput)) {
+            $command = $this->argvInput[0];
+            if($this->findCommand($command)) {
+                $this->commands->$command();
+            }
+            else {
+                print "Command not found.\n";
+                $this->commands->help();
+            }
+        }
+        else {
+            $this->commands->help();
+        }
+    }
+
+    /**
+     * Find aCommand command.
+     *
+     * @return bool
+     */
+    public function findCommand($command) {
+        return method_exists($this->commands, $command);
+    }
+}
