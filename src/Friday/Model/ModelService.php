673b744571ad9ae0e4a35b0a23f8532a6903b0ea
Gaurang Kumar <gaurangkumarp@gmail.com>
2018-09-10 00:43:19 +0530

    Add files via upload

---
+++ src/Friday/Model/ModelService.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * IronPHP : PHP Development Framework
+ * Copyright (c) IronPHP (https://github.com/IronPHP/IronPHP)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @package       IronPHP
+ * @copyright     Copyright (c) IronPHP (https://github.com/IronPHP/IronPHP)
+ * @link          
+ * @since         0.0.1
+ * @license       MIT License (https://opensource.org/licenses/mit-license.php)
+ * @auther        Gaurang Parmar <gaurangkumarp@gmail.com>
+ */
+
+namespace Friday\Model;
+
+class ModelService
+{
+    /**
+     * Instance of the Controller.
+     *
+     * @var \Friday\Controller\Controller
+     */
+    private static $controller;
+
+    /**
+     * Instance of the Application.
+     *
+     * @var \Friday\Foundation\Application
+     */
+    private static $app;
+
+    /**
+     * Instance of the DataMapper.
+     *
+     * @var \Friday\Model\DataMapper
+     */
+    private $dataMapper = null;
+
+    /**
+     * Create a new ModelService instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Initialize ModelService instance.
+     *
+     * @param  \Friday\Foundation\Application  $app
+     * @return void
+     */
+    public function initialize($app)
+    {
+        self::$app = $app;
+    }
+
+    /**
+     * Create Instance of Table.
+     *
+     * @param  string  $tableName
+     *
+     * @return \App\Model\Table
+     */
+    public function table($tableName)
+    {
+        if($this->dataMapper == null) {
+            $this->dataMapper = new \Friday\Model\DataMapper(self::$app->config);
+        }
+        return $this->dataMapper->getTable($tableName);
+    }
+
+}
