486b736490b4401a5b587f9b5c9d12f7849a24ec
Gaurang Kumar <gaurangkumarp@gmail.com>
2018-09-19 00:23:20 +0530

    get, post, register edited, view method added
    - get post methods have view arg
    - register method have view and data args
    - view method added to render view by View class

---
+++ Route.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * IronPHP : PHP Development Framework
+ * Copyright (c) IronPHP (https://github.com/IronPHP/IronPHP)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @package       IronPHP
+ * @copyright     Copyright (c) IronPHP (https://github.com/IronPHP/IronPHP)
+ * @link          
+ * @since         0.0.1
+ * @license       MIT License (https://opensource.org/licenses/mit-license.php)
+ * @auther        Gaurang Parmar <gaurangkumarp@gmail.com>
+ */
+
+namespace Friday\Http;
+
+class Route implements RouteInterface
+{
+    /**
+     * All registered route.
+     *
+     * @var array
+     */
+    public $routes;
+
+    /**
+     * Route instance.
+     *
+     * @var Route
+     */
+    public static $instance;
+
+    /**
+     * Create Route instance.
+     *
+     * @return void
+     */
+    public function __construct(/*$path, $controllerClass*/) {
+        //$this->path = $path;
+        //$this->controllerClass = $controllerClass;
+    }
+ 
+    /**
+     * register a GET method route.
+     *
+     * @param  string                $route
+     * @param  string|callback|null  $mix
+     * @param  string|null           $view
+     * @return bool
+     */
+    public function get($route, $mix = null, $view = null) {
+        self::$instance->register('GET', $route, $mix, $view);
+    }
+
+    /**
+     * register a POST method route.
+     *
+     * @param  string                $route
+     * @param  string|callback|null  $mix
+     * @param  string|null           $view
+     * @return bool
+     */
+    public function post($route, $mix = null, $view = null) {
+        self::$instance->register('POST', $route, $mix, $view);
+    }
+
+    /**
+     * register a GET method route with view.
+     *
+     * @param  string                $route
+     * @param  string|null           $view
+     * @param  array                 $data
+     * @return bool
+     */
+    public function view($route, $view = null, array $data = []) {
+        self::$instance->register('GET', $route, null, $view, $data);
+    }
+
+    /**
+     * register a route.
+     *
+     * @param  string                $route
+     * @param  string|callback|null  $mix
+     * @param  string|null           $view
+     * @param  array                 $data
+     * @return void
+     */
+    public function register($method, $route, $mix = null, $view = null, $data = []) {
+        $route = '/'.trim($route, '/');
+        self::$instance->routes[] = [$method, $route, $mix, $view, $data];
+    }
+
+    /**
+     * Match a uri route to registered routes.
+     *
+     * @param  object  $request  RequestInterface
+     * @return bool
+    public function match(RequestInterface $request) {
+        return $this->path === $request->getUri();
+    }
+     */
+ 
+    /**
+     * Create Controller instance.
+     *
+     * @return object
+    public function createController() {
+        return new $this->controllerClass;
+    }
+     */
+
+}
