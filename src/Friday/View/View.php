795199b8abd3b9f64f7e010026d93fe3da20534c
Gaurang Kumar <gaurangkumarp@gmail.com>
2018-09-21 23:39:31 +0530

    Add files via upload
    - tags and default arg value added to new vars
    - $tags for tags with true if tags in non empty and false if tag is empty
    - $attr for tag's default argument and value
    - $tempView created for temporary view
    - render method new - render full html page
    - renderHtml method new - to create set of tag
    - createTag method new - to create tag

--- src/Friday/View/View.php
+++ src/Friday/View/View.php
@@ -21,6 +21,58 @@ class View
 {
 
     /**
+     * All Tags used for rendering html page.
+     *
+     * @var array
+     */
+    private $tags = [
+        '!doctype' => false,
+        'html' => true,
+        'meta' => false,
+        'head' => true,
+        'body' => true,
+        'title' => true,
+        'p' => true,
+        'pre' => true,
+        'span' => true,
+        'div' => true,
+        'header' => true,
+        'footer' => true,
+        'script' => true,
+        'style' => true,
+        'link' => false,
+        'base' => false
+    ];
+
+    /**
+     * Attribute of Tags with default value used for rendering html page.
+     *
+     * @var array
+     */
+    private $attr = [
+        '!doctype' => ['html' => NULL],
+        'html' => ['lang' => 'en'],
+        'meta' => ['charset' => 'utf-8', 'name' => [
+                'content' => [
+                    "description" => "IronPHP Application",
+                    "keywords" => "IronPHP, framework",
+                    "author" => "Gaurang Kumar",
+                    "viewport" => "width=device-width, initial-scale=1.0"
+                ]
+            ]
+        ],
+        'base' => ['target' => '_blank'],
+        'link' => ['rel' => "stylesheet", 'type' => "text/css"]
+    ];
+
+    /**
+     * Temperory View.
+     *
+     * @var string
+     */
+    public $tempView = NULL;
+
+    /**
      * Name of the controller that created the View if any.
      *
      * @var string
@@ -200,6 +252,196 @@ class View
     }
 
     /**
+     * Renders view with HTML tags and given data, template file and layout.
+     *
+     * @param   array        $tags
+     * @return  void
+     */
+    public function addRenderTags($tags)
+    {
+        $i[0] = 0;
+        foreach($tags as $tag => $text) {
+            if(is_array($text)) {
+                $i[1] = 0;
+                foreach($text as $tag1 => $text1) {
+                    $temp[$i[0]][$i[1]] = $this->createTag($tag1, $text1);
+                }
+                $temp[$i[0]] = implode("\n", $temp[$i[0]]);
+            }
+            else {
+                $temp[$i[0]] = $this->createTag($tag, $text);
+            }
+            $i[0]++;
+        }
+        $this->tempView = implode("\n", $temp);
+        /*
+        {
+        $tags, $text = NULL, $parent = NULL, $child = NULL, $before = NULL, $after = NULL
+        $tempView = '<'.$tags;
+        if(isset($this->attr[$tags]) && $this->attr[$tags] != NULL) {
+            foreach($this->attr[$tags] as $attr => $val) {
+                $args[] = $attr.($val == NULL ? '' : "=\"$val\"");
+            }
+            $args = implode(' ', $args);
+        }
+        $tempView .= isset($args) ? ' '.$args.'>' : '>'; 
+        if($child != NULL) {
+            $tempView .= $text;
+        }
+        if($child != NULL) {
+            $tempView .= '{'.$child.'}';
+        }
+        if($this->tags[$tags] === true) {
+            $tempView .= "\n".'</'.$tags.'>';
+        }
+        if($after != NULL) {
+            $tempView = $tempView."\n".'@'.$after.'@';
+        }
+        if($parent != NULL) {
+            $tempView = str_replace('{'.$parent.'}', "\n".$tempView, $this->tempView);
+        }
+        if($before != NULL) {
+            $tempView = str_replace('@'.$before.'@', $tempView, $this->tempView);
+        }
+        $this->tempView = $tempView;
+        }
+        */
+    }
+
+    /**
+     * Renders view with HTML tags and given data, template file and layout.
+     *
+     * @param   string   $tag
+     * @return  array
+     */
+    public function createTag($tag, $attr = NULL, $content = NULL)
+    {
+        $temp = '<'.$tag;
+        if($attr != NULL) {
+            $attr = (array) $attr;
+            foreach($attr as $key => $val) {
+                if(is_numeric($key)) {
+                    $val = strtolower(trim($val));
+                    if(isset($this->attr[$tag][$val])) {
+                        $args[] = $val.($this->attr[$tag][$val] == NULL ? '' : "=\"{$this->attr[$tag][$val]}\"");
+                    }
+                    else {
+                        $args[] = $val;
+                    }
+                }
+                else {
+                    $args[] = $key.($val == NULL ? '' : "=\"$val\"");
+                    if(isset($this->attr[$tag][$key])) {
+                        foreach($this->attr[$tag][$key] as $k => $v) {
+                            if(isset($v[$val])) {
+                                $args[] = $k.($v[$val] == NULL ? '' : "=\"{$v[$val]}\"");
+                            }
+                        }
+                    }
+                }
+            }
+            $args = array_unique($args);
+            $args = implode(' ', $args);
+        }
+        $temp .= isset($args) ? ' '.$args.'>' : '>'; 
+        if($content != NULL) {
+            if(is_array($content)) {
+                array_walk($content, function(&$val, $key) { $val = trim($val); } );//5.3
+                $content = implode("\n",$content);
+                $content = explode("\n", $content);
+                $content = preg_replace('/^/', "\t", $content);
+                #$content = preg_filter('/^/', "@", $content);
+                $content = rtrim("\n".implode("\n", $content), "\n")."\n";
+            }
+            $temp .= $content;
+        }
+        if($this->tags[$tag] === true) {
+            $temp .= '</'.$tag.'>';
+        }
+        return $temp;
+
+        {
+        /*
+        $tags, $text = NULL, $parent = NULL, $child = NULL, $before = NULL, $after = NULL
+        $tempView = '<'.$tags;
+        if(isset($this->attr[$tags]) && $this->attr[$tags] != NULL) {
+            foreach($this->attr[$tags] as $attr => $val) {
+                $args[] = $attr.($val == NULL ? '' : "=\"$val\"");
+            }
+            $args = implode(' ', $args);
+        }
+        $tempView .= isset($args) ? ' '.$args.'>' : '>'; 
+        if($child != NULL) {
+            $tempView .= $text;
+        }
+        if($child != NULL) {
+            $tempView .= '{'.$child.'}';
+        }
+        if($this->tags[$tags] === true) {
+            $tempView .= "\n".'</'.$tags.'>';
+        }
+        if($after != NULL) {
+            $tempView = $tempView."\n".'@'.$after.'@';
+        }
+        if($parent != NULL) {
+            $tempView = str_replace('{'.$parent.'}', "\n".$tempView, $this->tempView);
+        }
+        if($before != NULL) {
+            $tempView = str_replace('@'.$before.'@', $tempView, $this->tempView);
+        }
+        $this->tempView = $tempView;
+        */
+        }
+    }
+
+    /**
+     * Renders view with HTML tags and given data, template file and layout.
+     *
+     * @param string|null  $viewPath
+     * @param string       $data
+     * @param string|null  $layout
+     * @return  $viewData.
+     */
+    public function renderHtml($viewData = null, $data = [], $layout = null)
+    {
+        $doctype = $this->createTag('!doctype'); //['HTML', 'PUBLIC', '"-//W3C//DTD HTML 4.01//EN"', '"http://www.w3.org/TR/html4/strict.dtd"']);
+        $charset = $this->createTag('meta', 'charset');
+        $viewport = $this->createTag('meta', ['name' => 'viewport']);
+        $title = $this->createTag('title', null, 'IronPHP');
+        $description = $this->createTag('meta', ['name' => 'description']);
+        $keywords = $this->createTag('meta', ['name' => 'keywords']);
+        $author = $this->createTag('meta', ['name' => 'author']);
+        $head = $this->createTag('head', null, [$charset, $viewport, $title, $description, $keywords, $author]);
+        $body = $this->createTag('body', null, [$viewData]);
+        $html = $this->createTag('html', null, [$head, $body]);
+        $dom = $doctype."\n".$html;
+        return $dom;
+
+        {
+        /*
+        $p1 = $this->createTag('p', null, 'Text');
+        $p2 = $this->createTag('p', null, 'Text');
+        $span1 = $this->createTag('span', null, [$p1, $p2]);
+        $span2 = $this->createTag('span', null, [$p1, $p2]);
+        $div1 = $this->createTag('div', null, [$span1, $span2]);
+        $div2 = $this->createTag('div', null, [$span1, $span2]);
+        $header = $this->createTag('header', null, [$div1, $div2]);
+        */
+
+        #$html = ['!doctype' => null, 'html' => ['head' => null]];
+        #$this->addRenderTags($html);
+
+        /*
+        $this->addRenderTags('!doctype', NULL, NULL, false, NULL, 'doc');
+        $this->addRenderTags('html', NULL, NULL, 'html', 'doc', NULL);
+        $this->addRenderTags('head', NULL, 'html', 'head', NULL, 'head');
+        $this->addRenderTags('body', NULL, NULL, NULL, 'head', NULL);
+        $this->addRenderTags('title', 'IronPHP', 'head', NULL, NULL, NULL);
+        */
+        }
+    }
+
+    /**
      * Renders view for given data, template file and layout.
      *
      * @param string|null  $viewPath
@@ -212,6 +454,9 @@ class View
         $viewData = file_get_contents($viewPath);
         foreach($data as $key => $val) {
             $viewData = str_replace('{{'.$key.'}}', $val, $viewData);
+        }
+        if($layout == null) {
+            $viewData = $this->renderHtml($viewData);
         }
         return $viewData;
         /*
