dfed5bee2d05f3d35391f98fbb1946e5cae83c7c
Gaurang Kumar <gaurangkumarp@gmail.com>
2018-10-10 12:46:55 +0530

    just refresh

--- src/Friday/Helper/Function.php
+++ src/Friday/Helper/Function.php
@@ -17,11 +17,6 @@
 
 use Friday\Foundation\Application;
 
-/**
- * Get enviroment variables value.
- *
- * @return mix
- */
 if (! function_exists('env')) {
     /**
      * Gets the value of an environment variable. Supports boolean, empty and null.
@@ -146,82 +141,52 @@ if (! function_exists('ends_with')) {
     }
 }
 
-/**
- * create pagination.
- *
- * @param  int     $total
- * @param  int     $per_page  =  10
- * @param  int     $page      =  1
- * @param  string  $url
- *
- * @return mix
- */
-function pagination($total, $per_page = 10,$page = 1, $url){
-	$adjacents = "2"; 
-    	
-	$page = ($page == 0 ? 1 : $page);  
-    $start = ($page - 1) * $per_page;								
-	
-    $prev = $page - 1;							
-    $next = $page + 1;
-    $lastpage = ceil($total/$per_page);
-    $lpm1 = $lastpage - 1;
-
-    $pagination = "";
-    if($lastpage >= 1){	
-		$pagination .= "<ul class='pagination'>";
-        $pagination .= "<li class='details'><a>$page/$lastpage</a></li>";
-
-		if($lastpage < 2 + ($adjacents * 2)){ // $lastpage < 6
-			for($counter = 1; $counter <= $lastpage; $counter++){
-				if ($counter == $page)
-					$pagination.= "<li class='active'><a>$counter</a></li>";
-    			else
-    				$pagination.= "<li><a href='{$url}page=$counter'>$counter</a></li>";					
-    		}
-    	}elseif($lastpage > 2 + ($adjacents * 2)){ // $lastpage > 6
-			if($page < 0 + ($adjacents * 2)){ // $page < 4
-				for($counter = 1; $counter < 2 + ($adjacents * 2); $counter++){
-					if($counter == $page)
-    					$pagination.= "<li class='active'><a>$counter</a></li>";
-    				else
-    					$pagination.= "<li><a href='{$url}page=$counter'>$counter</a></li>";					
-    			}
-				$pagination.= "<li><a href='{$url}page=$lastpage'>&raquo;</a></li>";
-    		}elseif($lastpage - ($adjacents * 1) > $page && $page > ($adjacents * 1)){ // $lastpage - 2 > $page && $page > 3
-    			$pagination.= "<li><a href='{$url}page=1'>&laquo;</a></li>";
-    			for($counter = $page - $adjacents; $counter <= $page + $adjacents; $counter++){
-    				if($counter == $page)
-    					$pagination.= "<li class='active'><a>$counter</a></li>";
-    				else
-    					$pagination.= "<li><a href='{$url}page=$counter'>$counter</a></li>";					
-    			}
-    			$pagination.= "<li><a href='{$url}page=$lastpage'>&raquo;</a></li>";	
-    		}else{
-    			$pagination.= "<li><a href='{$url}page=1'>&laquo;</a></li>";
-    			for($counter = $lastpage - ($adjacents * 2); $counter <= $lastpage; $counter++){
-					if($counter == $page)
-    					$pagination.= "<li class='active'><a>$counter</a></li>";
-    				else
-    					$pagination.= "<li><a href='{$url}page=$counter'>$counter</a></li>";					
-    			}
-    		}
-    	}else{
-    		if($page < 1 + ($adjacents * 2)){
-				for($counter = 1; $counter < 2 + ($adjacents * 2); $counter++){
-					if($counter == $page)
-    					$pagination.= "<li class='active'><a>$counter</a></li>";
-    				else
-    					$pagination.= "<li><a href='{$url}page=$counter'>$counter</a></li>";					
-				}
-				$pagination.= "<li><a href='{$url}page=$lastpage'>&raquo;</a></li>";
-			}
-		}
-		$pagination.= "</ul>\n";		
-	}
-	return $pagination;
+if (! function_exists('is_session_started')) {
+    /**
+     * Check if session have been started.
+     *
+     * @return bool
+     */
+    function is_session_started()
+    {
+	    if ( PHP_SAPI !== 'cli' ) {
+		    if ( version_compare(PHP_VERSION, '5.4.0', '>=') ) {
+			    return (session_status() === PHP_SESSION_ACTIVE) ? true : false;
+            }
+            else {
+			    return (session_id() === '') ? false : true;
+            }
+	    }
+	    return false;
+    }
 }
 
+if (! function_exists('is_bot')) {
+    /**
+     * Check whether the visitor is a search engine robot.
+     *
+     * @return  bool
+     */
+    function is_bot() {
+	    $botlist = array("Teoma", "alexa", "froogle", "Gigabot", "inktomi",
+	    "looksmart", "URL_Spider_SQL", "Firefly", "NationalDirectory",
+	    "Ask Jeeves", "TECNOSEEK", "InfoSeek", "WebFindBot", "girafabot",
+	    "crawler", "www.galaxy.com", "Googlebot", "Scooter", "Slurp",
+	    "msnbot", "appie", "FAST", "WebBug", "Spade", "ZyBorg", "rabaz",
+	    "Baiduspider", "Feedfetcher-Google", "TechnoratiSnoop", "Rankivabot",
+	    "Mediapartners-Google", "Sogou web spider", "WebAlta Crawler","TweetmemeBot",
+	    "Butterfly","Twitturls","Me.dium","Twiceler");
+
+    	foreach($botlist as $bot){
+	    	if(strpos($_SERVER['HTTP_USER_AGENT'], $bot) !== false) {
+		        return true;	// Is a bot
+            }
+	    }
+	    return false;	// Not a bot
+    }
+}
+
+/*
 function moneyInWords($m){
 	if($m>=1000){
 		if($m>=100000){
@@ -307,17 +272,6 @@ function qry_arg($arg,$conj){
 	}
 	return $args;
 }
-function clean($str) {
-	/*
-	$str = @trim($str);
-	if(get_magic_quotes_gpc()) {
-		$str = stripslashes($str);
-	}
-	return mysql_real_escape_string($str);
-	*/
-	return $str;
-}
-/*
 ?>
 <script>
 function setCookie(cookieName, cookieValue, expdays) {
@@ -346,4 +300,3 @@ function getCookie(cookieName) {
 </script>
 <?php
 */
-?>
