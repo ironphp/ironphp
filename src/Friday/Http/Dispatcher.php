64e240a4f553187d3e1eda1f0755dc11333fc937
Gaurang Kumar <gaurangkumarp@gmail.com>
2018-10-13 23:42:29 +0530

    dispatch() improved
    dispatch method now return output+return
    and controller ,view as assoc array

--- src/Friday/Http/Dispatcher.php
+++ src/Friday/Http/Dispatcher.php
@@ -41,11 +41,11 @@ class Dispatcher
         if(isset($route[3]) && is_string($route[3])) {
             $view = $route[3];
             $data = $route[4];
-            return ['view', $view, $data];
+            return ['view' => [$view, $data]];
         }
         elseif(isset($route[2]) && is_string($route[2])) {
             list($controller, $method) = explode('@', $route[2]);
-            return ['controller_method', $controller, $method];
+            return ['controller' => [$controller, $method]];
         }
         elseif(isset($route[2]) && ($route[2] instanceof Closure || get_class($route[2]) === "Closure")) {
             $function = $route[2];
@@ -56,14 +56,14 @@ class Dispatcher
             }
             ob_start();
             if($request->getParam('Closure') !== false && is_array($request->getParam('Closure')) ) {
-                call_user_func_array($route[2], $request->getParam('Closure'));
+                $return = call_user_func_array($route[2], $request->getParam('Closure'));
             }
             else {
                 //$function();
                 throw new \Exception('Invaliding parameter passed in route\'s callable function: '.$route[1]);
             }
             $output = ob_get_clean();
-            return ['output', $output];
+            return ['output' => [$output, $return]];
         }
         else {
             throw new \Exception('Invaliding route is registered for: '.$route[1]);
