d24584e50b6d80ec9413516cef9d478ecd5b621c
Gaurang Kumar <gaurangkumarp@gmail.com>
2018-11-22 23:29:19 +0530

    keycommand class created
    - to generate secure application key: APP_KEY

---
+++ src/Friday/Console/Command/KeyCommand.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * IronPHP : PHP Development Framework
+ * Copyright (c) IronPHP (https://github.com/IronPHP/IronPHP)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @package       IronPHP
+ * @copyright     Copyright (c) IronPHP (https://github.com/IronPHP/IronPHP)
+ * @link          
+ * @since         0.0.1
+ * @license       MIT License (https://opensource.org/licenses/mit-license.php)
+ * @auther        Gaurang Parmar <gaurangkumarp@gmail.com>
+ */
+
+namespace Friday\Console\Command;
+
+use Friday\Console\Colors;
+use Friday\Console\Command;
+use Friday\Foundation\Application;
+
+class KeyCommand extends Command
+{
+    /**
+     * Command options.
+     *
+     * @var array
+     */
+    private $option;
+
+    /**
+     * Create a List Commands instance.
+     *
+     * @param array  $option
+     * @return void
+     */
+    public function __construct($option = [])
+    {
+        $this->option = $option;
+    }
+
+    /**
+     * Execute Commands.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $output = PHP_EOL.$this->key().Colors::BG_BLACK.Colors::WHITE;
+        return $output;
+    }
+
+    /**
+     * Generate new key.
+     *
+     * @return void
+     */
+    public function key() {
+        $set = $this->getConsole()->setKey();
+        return $set ? Colors::GREEN."Application key set successfully." : Colors::RED."Error: Key is not set.";
+    }
+}
