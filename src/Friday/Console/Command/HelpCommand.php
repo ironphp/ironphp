846026174208fba31675c6f297306919bb60e065
Gaurang Kumar <gaurangkumarp@gmail.com>
2018-11-25 23:13:57 +0530

    help for another command added

--- src/Friday/Console/Command/HelpCommand.php
+++ src/Friday/Console/Command/HelpCommand.php
@@ -46,19 +46,46 @@ class HelpCommand extends Command
      *
      * @return void
      */
-    public function run($command = 'help')
+    public function run()
+    {
+        if(\count($this->option) === 0) {
+            return $this->help();
+        }
+        else {
+            return $this->executeHelp($this->option[0]);
+        }
+    }
+
+    /**
+     * Help for this Commands.
+     *
+     * @param  string  $command
+     * @return void
+     */
+    public function help($command = 'help')
     {
         $output = Colors::LIGHT_BLUE.str_repeat("-", 63).PHP_EOL.
-        Colors::GREEN."IronPHP".Colors::WHITE." Framework ".Colors::YELLOW."".Application::VERSION.Colors::WHITE." (kernel: src, env: ".env('APP_ENV').", debug: ".env('APP_DEBUG').")".PHP_EOL.
+        Colors::GREEN."IronPHP ".Colors::WHITE."Framework ".Colors::YELLOW.Application::VERSION.Colors::WHITE." (kernel: src, env: ".env('APP_ENV').", debug: ".env('APP_DEBUG').")".PHP_EOL.
         Colors::LIGHT_BLUE.str_repeat("-", 63).PHP_EOL.
+        Colors::YELLOW."Description:".PHP_EOL.
+        Colors::WHITE."  Displays help for a command".PHP_EOL.
+        PHP_EOL.
         Colors::YELLOW."Usage:".PHP_EOL.
-        Colors::WHITE."  $command [options] [--] [<command_name>]".PHP_EOL.PHP_EOL.
+        Colors::WHITE."  help [options] [--] [<command_name>]".PHP_EOL.
+        PHP_EOL.
+        Colors::YELLOW."Arguments:".PHP_EOL.
+        Colors::GREEN."  command ".Colors::WHITE."\tThe command to execute".PHP_EOL.
+        Colors::GREEN."  command_name ".Colors::WHITE."\tThe command name ".Colors::YELLOW."[default: \"help\"]".PHP_EOL.
+        PHP_EOL.
         Colors::YELLOW."Options:".PHP_EOL.
         Colors::GREEN."  -h, --help".Colors::WHITE."\t\tDisplay this help message".PHP_EOL.
-        Colors::GREEN."  -V, --version".Colors::WHITE."\t\tDisplay this application version".PHP_EOL.PHP_EOL.
+        Colors::GREEN."  -V, --version".Colors::WHITE."\t\tDisplay this application version".PHP_EOL.
+        PHP_EOL.
         Colors::YELLOW."Help:".PHP_EOL.
-        Colors::WHITE."  The ".Colors::GREEN."help".Colors::WHITE." command displays help for a given command:".PHP_EOL.PHP_EOL.
-        Colors::GREEN."    php jarvis help".PHP_EOL.PHP_EOL.
+        Colors::WHITE."  The ".Colors::GREEN."help".Colors::WHITE." command displays help for a given command:".PHP_EOL.
+        PHP_EOL.
+        Colors::GREEN."    php jarvis help".PHP_EOL.
+        PHP_EOL.
         Colors::WHITE."  To display the list of available commands, please use the ".Colors::GREEN."list".Colors::WHITE." command.".PHP_EOL.
         Colors::BG_BLACK.Colors::WHITE;
         return $output;
