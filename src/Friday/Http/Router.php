a8c3d9d1bfae8d9a092ce69a6ec8d5c158b0b476
Gaurang Kumar <gaurangkumarp@gmail.com>
2018-11-28 21:10:51 +0530

    version 1.0.0

--- src/Friday/Http/Router.php
+++ src/Friday/Http/Router.php
@@ -10,7 +10,7 @@
  * @package       IronPHP
  * @copyright     Copyright (c) IronPHP (https://github.com/IronPHP/IronPHP)
  * @link          
- * @since         0.0.1
+ * @since         1.0.0
  * @license       MIT License (https://opensource.org/licenses/mit-license.php)
  * @auther        GaurangKumar Parmar <gaurangkumarp@gmail.com>
  */
