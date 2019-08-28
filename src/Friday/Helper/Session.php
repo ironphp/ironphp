da348e42177d5d8c6b0fbb908f616fc20fde04ea
Gaurang Kumar <gaurangkumarp@gmail.com>
2019-08-28 23:37:53 +0530

    phpstan Session fixed

--- src/Friday/Helper/Session.php
+++ src/Friday/Helper/Session.php
@@ -7,9 +7,9 @@
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) IronPHP (https://github.com/IronPHP/IronPHP)
+ * @copyright     Copyright (c) IronPHP
  *
- * @link
+ * @link		  https://github.com/IronPHP/IronPHP
  * @since         1.0.0
  *
  * @license       MIT License (https://opensource.org/licenses/mit-license.php)
@@ -189,6 +189,6 @@ class Session
      */
     public function checkToken($token)
     {
-        return isset($token) && $token === $token;
+        return !empty($token) && $token === $token;
     }
 }
