62d91a342ea6ab6c050765c7d7cd2179662ccd83
Gaurang Kumar <gaurangkumarp@gmail.com>
2019-09-08 21:44:53 +0530

    heading to 1.0.5-alpha3

--- src/Friday/Foundation/Application.php
+++ src/Friday/Foundation/Application.php
@@ -35,7 +35,7 @@ class Application
      *
      * @var string
      */
-    const VERSION = '1.0.5-alpha1';
+    const VERSION = '1.0.5-alpha3';
 
     /**
      * The base path for the IronPHP installation.
