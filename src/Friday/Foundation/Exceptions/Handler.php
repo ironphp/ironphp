5a63731b0f55a23da2f507369dcf7456dfe6abd8
Gaurang Kumar <gaurangkumarp@gmail.com>
2019-09-14 12:57:51 +0530

    handler updated

--- src/Friday/Foundation/Exceptions/Handler.php
+++ src/Friday/Foundation/Exceptions/Handler.php
@@ -198,58 +198,58 @@ class Handler implements HandlerInterface
         if (method_exists($exception, 'getSeverity')) {
             $severityCode = $exception->getSeverity();
             switch ($severityCode) {
-            case E_ERROR: // 1 //
-                $severity = 'E_ERROR';
-                break;
-            case E_WARNING: // 2 //
-                $severity = 'E_WARNING';
-                break;
-            case E_PARSE: // 4 //
-                $severity = 'E_PARSE';
-                break;
-            case E_NOTICE: // 8 //
-                $severity = 'E_NOTICE';
-                break;
-            case E_CORE_ERROR: // 16 //
-                $severity = 'E_CORE_ERROR';
-                break;
-            case E_CORE_WARNING: // 32 //
-                $severity = 'E_CORE_WARNING';
-                break;
-            case E_COMPILE_ERROR: // 64 //
-                $severity = 'E_COMPILE_ERROR';
-                break;
-            case E_COMPILE_WARNING: // 128 //
-                $severity = 'E_COMPILE_WARNING';
-                break;
-            case E_USER_ERROR: // 256 //
-                $severity = 'E_USER_ERROR';
-                break;
-            case E_USER_WARNING: // 512 //
-                $severity = 'E_USER_WARNING';
-                break;
-            case E_USER_NOTICE: // 1024 //
-                $severity = 'E_USER_NOTICE';
-                break;
-            case E_STRICT: // 2048 //
-                $severity = 'E_STRICT';
-                break;
-            case E_RECOVERABLE_ERROR: // 4096 //
-                $severity = 'E_RECOVERABLE_ERROR';
-                break;
-            case E_DEPRECATED: // 8192 //
-                $severity = 'E_DEPRECATED';
-                break;
-            case E_USER_DEPRECATED: // 16384 //
-                $severity = 'E_USER_DEPRECATED';
-                break;
-            case E_ALL: // 32767 //
-                $severity = 'E_ALL';
-                break;
-            default:
-                $severity = 'UNKOWN';
-                break;
-        }
+				case E_ERROR: // 1 //
+					$severity = 'E_ERROR';
+					break;
+				case E_WARNING: // 2 //
+					$severity = 'E_WARNING';
+					break;
+				case E_PARSE: // 4 //
+					$severity = 'E_PARSE';
+					break;
+				case E_NOTICE: // 8 //
+					$severity = 'E_NOTICE';
+					break;
+				case E_CORE_ERROR: // 16 //
+					$severity = 'E_CORE_ERROR';
+					break;
+				case E_CORE_WARNING: // 32 //
+					$severity = 'E_CORE_WARNING';
+					break;
+				case E_COMPILE_ERROR: // 64 //
+					$severity = 'E_COMPILE_ERROR';
+					break;
+				case E_COMPILE_WARNING: // 128 //
+					$severity = 'E_COMPILE_WARNING';
+					break;
+				case E_USER_ERROR: // 256 //
+					$severity = 'E_USER_ERROR';
+					break;
+				case E_USER_WARNING: // 512 //
+					$severity = 'E_USER_WARNING';
+					break;
+				case E_USER_NOTICE: // 1024 //
+					$severity = 'E_USER_NOTICE';
+					break;
+				case E_STRICT: // 2048 //
+					$severity = 'E_STRICT';
+					break;
+				case E_RECOVERABLE_ERROR: // 4096 //
+					$severity = 'E_RECOVERABLE_ERROR';
+					break;
+				case E_DEPRECATED: // 8192 //
+					$severity = 'E_DEPRECATED';
+					break;
+				case E_USER_DEPRECATED: // 16384 //
+					$severity = 'E_USER_DEPRECATED';
+					break;
+				case E_ALL: // 32767 //
+					$severity = 'E_ALL';
+					break;
+				default:
+					$severity = 'UNKOWN';
+					break;
+			}
         }
         if (env('APP_DEBUG') === true) {
             echo '
